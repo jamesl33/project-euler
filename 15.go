721362def722f39a34333a0c1ed896153da0124a
James Lee <jamesl33info@gmail.com>
2021-04-11 20:33:21 +0100

    Relicense 'GPLv3' -> 'Unlicense'

--- 15.go
+++ 15.go
@@ -1,20 +1,3 @@
-/* This file is part of project-euler
-
-Copyright (C) 2019, James Lee <jamesl33info@gmail.com>.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
 // Mathimatical solution using Binomial coefficient. https://en.wikipedia.org/wiki/Binomial_coefficient
 // With help from https://github.com/nayuki/Project-Euler-solutions/blob/master/python/p015.py
 
